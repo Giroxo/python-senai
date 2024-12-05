aa2eb2b18dffd4f0fbb52ca6536205ed173b8522
Giroxo <giovana.roxo2004@gmail.com>
2024-12-05 09:55:46 -0300

    ADD AULA 02

---
+++ AULA 2/aula02.py
@@ -0,0 +1,5 @@
+nome_usuario = ´Giovana Roxo´ # tipo -> str = string
+idade_usuario = 19 # tipo -> int = interger 
+altura_usuario = 1.58 # tipo -> float = ponto flutuante 
+maior_idade = True 
+print (´seja bem-vindo´ , nome_usuario , sua idade é idade_usuario anos, você tem altura_usuario metros. Você é maior de idade? maior_idade)
